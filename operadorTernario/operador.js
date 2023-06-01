22c1cb6f1453de712fa1df34a2377055571b005f
SergioTanakaAtos <sergio.tanaka@atos.net>
2023-06-01 10:50:01 -0300

    My first commit

---
+++ operadorTernario/operador.js
@@ -0,0 +1,7 @@
+const velocidade = 75;
+const warn = 85;
+
+// condicao ? true ? false 
+const condicao = velocidade >= warn 
+? console.log("Ultrapassou o limite"):
+console.log("Continue andando");
